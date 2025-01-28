e185c40a0b8a2f455348a09bb1ace45db5355480
Nikita Sukhankin <suhankin.n@yandex.ru>
2025-01-28 23:12:57 +0700

    Первое ревью

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Nikita-Sukhankin/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
 // @todo: Темплейт карточки
-
-// @todo: DOM узлы
+const galleryContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: Функция создания карточки
+function createCard(name, link) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+    // Ссылка на изображение
+    const cardImage = cardElement.querySelector('.card__image');
+    
+    // Заполняем данные
+    cardElement.querySelector('.card__title').textContent = name;
+    cardImage.setAttribute('src', link);
+    cardImage.setAttribute('alt', name);
+
+    // Обработчик клика по корзине удаления
+    const buttonDelete = cardElement.querySelector('.card__delete-button');
+    buttonDelete.addEventListener('click', handDeleteCard);
+
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function handDeleteCard(event) {
+    const card = event.target.closest('.places__item');
+    card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function renderCard(cardElement) {
+    galleryContainer.append(cardElement);
+}
+
+
+initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData.name, cardData.link);
+    renderCard(cardElement);
+});
